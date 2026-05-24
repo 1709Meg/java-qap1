936a63dc06e1a8a19bff50f98a6d052cc1298056
1709Meg <meghickey@yahoo.com>
2026-05-24 13:58:02 -0230

    Finished Problem 2

--- Problem1/TestAccount.java
+++ Problem1/TestAccount.java
@@ -1,3 +1,18 @@
-public class TestAccount {
+package problem1;
 
+public class TestAccount {
+    public static void main(String[] args) {
+
+        Account acct1 = new Account("1", "acct1", 5000);
+
+        Account acct2 = new Account("2", "acct2", 4000);
+
+        System.out.println(acct1.getBalance());
+        System.out.println(acct2.getBalance());
+
+        acct1.transferTo(acct2, 1000);
+
+        System.out.println(acct1.getBalance());
+        System.out.println(acct2.getBalance());
+    }
 }
---
+++ problem2/TestDate.java
@@ -0,0 +1,10 @@
+package problem2;
+
+public class TestDate {
+    public static void main(String[] args) {
+
+        Date date1 = new Date(3, 5, 2026);
+
+        System.out.println(date1.toString());
+    }
+}
